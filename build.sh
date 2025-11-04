6c74b472fbabe513f5b8b2e2fb398b96cbb49b1f
Igor Dejanovic <igor.dejanovic@gmail.com>
2025-11-04 10:12:39 +0100

    побољшање скрипте за изградњу

--- build.sh
+++ build.sh
@@ -2,11 +2,14 @@
 rm -fr build
 mkdir build
 TARGETS=(x86_64-unknown-linux-gnu aarch64-apple-darwin x86_64-pc-windows-gnu)
+RELEASE=`git describe --tags --abbrev=0`
 for TARGET in "${TARGETS[@]}"; do
     cargo build --profile release --target "$TARGET"
+    OUTPUT="build/${TARGET}-${RELEASE}.zip"
     if [[ "$TARGET" != *"windows"* ]]; then
-        zip -j "build/${TARGET}.zip" "target/$TARGET/release/ћирко"
+        zip -j "$OUTPUT" "target/$TARGET/release/ћирко"
     else
-        zip -j "build/${TARGET}.zip" "target/$TARGET/release/ћирко.exe"
+        zip -j "$OUTPUT" "target/$TARGET/release/ћирко.exe"
     fi
+    gpg --armor --detach-sign "$OUTPUT"
 done
